b8290513e86ef90f214f3b296cd64357fa1a5bad
Parth Singla <parthsingla071204@gmail.com>
2026-06-25 16:09:29 +0530

    feat: Day 1 - first Python program

---
+++ day1.py
@@ -0,0 +1,14 @@
+print ("Hello World")
+print ("my name is Parth")
+print ("i will become engineer")
+
+name = "Parth"
+age = 21
+print (name)
+print (age)
+user_name = input("Tumhara naam kya hai? ")
+print("Hello " + user_name)
+user_name = input("Tumhari age kya hai? ")
+print("21 age" + user_name)
+user_age = input("Tumhari age kya hai? ")
+print("Parth ki age " + user_age + " hai")
